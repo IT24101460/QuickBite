a94e9724766f1161d849aa02bd7694416894160e
nrajdev <it24101460@my.sliit.lk>
2026-04-26 23:34:27 +0530

    progress added,but need improve UI to userfriendly

--- ReadMe files/login_details.md
+++ ReadMe files/login_details.md
@@ -5,6 +5,10 @@
 
 
 
+<<<<<<< HEAD
 owner1@canteen.com
 owner12345
 
+=======
+owner1@canteen.com
+>>>>>>> 47c8e939 (progress added,but need improve UI to userfriendly)
--- server/config/supabase.js
+++ server/config/supabase.js
@@ -1,11 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Using the credentials you provided
+<<<<<<< HEAD
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or Key is missing. Check .env file.");
 }
+=======
+const supabaseUrl = 'https://fjedogqftcjvcptkygwb.supabase.co';
+const supabaseKey = 'sb_secret_4tR-Pb8Da2n1lh6kIK02rQ_ktfHW2DV';
+>>>>>>> 47c8e939 (progress added,but need improve UI to userfriendly)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
